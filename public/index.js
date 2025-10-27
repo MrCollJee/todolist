a5b4a23bcc2ad561d1664d0b36e94dd2d81ae5ee
Mr Coll Jee <mrcolljee@example.com>
2025-10-27 15:01:36 +0100

    Ajout des fichiers html, css et js de base

---
+++ public/index.js
@@ -0,0 +1,35 @@
+/**
+ * Gestion de l'ajout d'une tâche via le champ de texte
+ */
+function handleAddTaskForm(){
+    const addTaskForm = document.querySelector(".addTask");
+    const textareaTask = document.querySelector(".addTask__textarea");
+    const taskList = document.querySelector(".taskList");
+
+    addTaskForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const taskContent = textareaTask.value // Récupération de la tâche dans le champ de texte
+        
+        newTask(taskContent);
+    })
+}
+
+handleAddTaskForm();
+
+/**
+ * Création d'une nouvelle tâche dans la liste des tâches
+ * @param {string} taskContent : Le contenu de la tâche
+ * @param {boolean} isDone : Indique si la tâche est terminée ou non (par défaut false)
+ */
+function newTask(taskContent, isDone = false){
+    const newTask = document.createElement("p");
+    const taskList = document.querySelector(".taskList");
+
+    newTask.classList.add("taskList__task");
+    if(isDone){
+        newTask.classList.add("taskList__task--done");
+    }
+    newTask.textContent = taskContent;
+
+    taskList.appendChild(newTask);  
+}
