751273cdeb8f5fe62e6856d9c3b489603859d65a
Mr Coll Jee <mrcolljee@example.com>
2025-11-06 15:52:25 +0100

    Ajout des boutons supprimer pour une tâche et tout supprimer

--- public/src/functions.js
+++ public/src/functions.js
@@ -1,10 +1,13 @@
+//CONSTANTES
+const taskListDOM = document.querySelector(".taskList");
+
 /**
  * Affiche au chargement de la page les tâches stockées dans le local Storage
  */
 export function showTasks(){
     const taskListStorage = JSON.parse(window.localStorage.getItem("taskList")); // Récupération de la liste des tâche dans le localStorage
     taskListStorage.forEach(task => {
-        addNewTask(task.content, task.isDone, false);
+        newTask(task.content, task.isDone, false);
     });
 }
 
@@ -19,7 +22,7 @@ export function handleAddTaskForm(){
         event.preventDefault();
         const taskContent = textareaTask.value // Récupération de la tâche dans le champ de texte
 
-        addNewTask(taskContent);
+        newTask(taskContent);
     })
 }
 
@@ -29,35 +32,45 @@ export function handleAddTaskForm(){
  * @param {boolean} isDone : Indique si la tâche est terminée ou non (par défaut nan)
  * @param {boolean} toLocalStorage : Indique si l'on stocke la tâche dans le local Storage (par défaut oui)
  */
-function addNewTask(taskContent, isDone = false, toLocalStorage = true){
+function newTask(taskContent, isDone = false, toLocalStorage = true){
     //Ajout de la tâche dans le DOM
-    const taskListDOM = document.querySelector(".taskList");
-    
-    // let taskContainer = document.createElement("div");
+    let taskContainer = document.createElement("div")
     let task = document.createElement("p");
-    // let taskRemover = document.createElement("button");
+    let taskRemover = document.createElement("button");
 
-    // taskContainer.classList.add("taskList__task")
+    taskContainer.classList.add("taskContainer");
 
     task.textContent = taskContent;
     task.classList.add("taskList__task");
-
-    // taskRemover.value = "Supprimer";
-    // taskRemover.classList.add("task__removeButton");
-
     if(isDone){
         task.classList.add("taskList__task--done");
     }
 
-    // taskContainer.appendChild(task);
-    // taskContainer.appendChild(taskRemover);
+    taskRemover.innerText = "Supprimer";
+    taskRemover.classList.add("taskList__removeTaskButton");
 
-
-    taskListDOM.appendChild(task);
+    taskContainer.appendChild(task);
+    taskContainer.appendChild(taskRemover);
+    taskListDOM.appendChild(taskContainer);
 
     // Ajout de l'événement de clic pour marquer la tâche comme terminée
     task.addEventListener("click", (event) => {
         event.target.classList.toggle("taskList__task--done");
+    })
+
+    // Ajout de l'évènement de suppression
+    taskRemover.addEventListener("click", (event) => {
+        if(window.confirm("Etes-vous sûr de vouloir supprimer cette tâche ?")){
+            //Suppression du localStorage
+            const taskListStorage = JSON.parse(window.localStorage.getItem("taskList")); //Récupération de taskList dans le local Storage
+            const ArrayTaskList = Array.from(taskListDOM.children);// Convertir la collection HTMLCollection en Array pour utiliser indexOf
+            const taskIndex = ArrayTaskList.indexOf(event.target.parentNode);// On récupère l'index du container dans la liste
+            
+            taskListStorage.splice(taskIndex - 1, 1); //taskIndex - 1 => Décalage entre TaskListDOM et Storage car ArrayTaskList contient le titre en plus
+            window.localStorage.setItem("taskList", JSON.stringify(taskListStorage)); 
+
+            taskListDOM.removeChild(event.target.parentNode); 
+        }
     })
 
     //Ajout conditionnel de la tâche dans le localStorage
